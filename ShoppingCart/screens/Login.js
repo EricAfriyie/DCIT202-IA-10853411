bbab005a9809849dabf407559a61722cd374a530
EricAfriyie <62954159+EricAfriyie@users.noreply.github.com>
2021-10-29 20:06:40 +0000

    Update Login.js
    loginfirst update

--- ShoppingCart/screens/Login.js
+++ ShoppingCart/screens/Login.js
@@ -5,29 +5,30 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen'; 
-import ImagedCarouselCard from "react-native-imaged-carousel-card"; 
-import PaperOnboarding, {PaperOnboardingItemType} from "@gorhom/paper-onboarding";
+
 
 export default function Login(params) {
   const navigation = params.navigation;
   return (
     <View style={{ flex:1}}>
     <View>
-     <Text style={{ color: '#080A50', fontWeight:"bolder",fontSize:43,textAlign:"center",marginTop:10}}>Kew<Text style={{color:"#f03e3e",fontSize:43}}>s</Text></Text></View>
+     <Text style={{ color: '#080A50', fontFamily: 'monospace',fontWeight:"bolder",fontSize:43,textAlign:"center"}}>Kew<Text style={{color:"#f03e3e",fontSize:43}}>s</Text></Text></View>
+  
+    
         <StatusBar hidden={true} />
         <View style={{flexDirection:"row"}}>
-         <View style={styles.container2}>
-        <Image source={require('./pngegg.png')} style={{width:20,height:20, alignSelf:"center", marginTop:10,}}/>
-        
-</View>
-        <View style={styles.container2}>
-        <Image source={require('./pngegg.png')} style={{width:20,height:20, alignSelf:"center", marginTop:10,}}/>
-        </View></View>
-        <View style={styles.container1}>
-        <Image source={require('./pngegg.png')} style={{width:300,height:230, alignSelf:"center", marginTop:30}}/>
-        </View>
-       
 
+         <View style={styles.container2} >
+        <Image source={require('./pictures/phonesale.png')} style={{width:50,height:70,marginBottom:40, alignSelf:"center",marginLeft:40}}/>
+        </View>
+
+        <View style={styles.container3}>
+        <Image source={require('./pictures/phonerepair1.png')} style={{width:120,height:75,marginRight:30, alignSelf:"center", }}/>
+        </View></View>
+
+        <View style={styles.container1}>
+        <Image source={require('./pictures/pngegg.png')} style={{width:305,height:230, alignSelf:"center", marginTop:30}}/>
+        </View>
 
         <Text style={{ color: '#080A50', fontWeight:"bolder",fontSize:30,textAlign:"center"}}>Find your latest &</Text>
         <Text style={{ color: '#080A50', fontWeight:"bolder",fontSize:20,textAlign:"center"}}> Stylish Gadgets here </Text>
@@ -65,19 +66,31 @@ const styles = StyleSheet.create({
     height: hp('35%'),
     width: wp('74%'),
    alignSelf:"center",
-    marginTop:30,
+    marginTop:20,
     marginBottom:90,
     borderRadius:500,
+   
   },
-
-
-
 
    container2: {
     backgroundColor: '#080A50',
-    height: hp('5%'),
-    width: wp('10%'),
-    alignSelf:"center",
+    height: hp('7%'),
+    width: wp('14%'),
+    marginRight:150,
+    marginLeft:50,
+   alignSelf:"center",
     borderRadius:500,
+   
+  },
+
+   container3: {
+    backgroundColor: '#080A50',
+    height: hp('7%'),
+    width: wp('14%'),
+    marginTop:60,
+   alignSelf:"center",
+    borderRadius:500,
+   
   }
+  
 });
