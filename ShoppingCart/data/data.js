8c13fff85f59a14fa59524dc648f5daff55b740e
Eric Afriyie <62954159+EricAfriyie@users.noreply.github.com>
2021-11-17 14:17:48 -0800

    Add files via upload

---
+++ ShoppingCart/data/data.js
@@ -0,0 +1,18 @@
+export const dummyData =
+        [{
+                title: 'Cool Chops', url: 'https://www.gizbot.com/img/2021/03/amazon-laptop-days-sale-discount-offers-on-best-laptops-1616151820.jpg',
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                id: 1
+
+        },
+        {
+                title: 'Affordables', url: 'https://fdn.gsmarena.com/imgroot/news/20/11/huawei-black-friday-sales/-1200/gsmarena_001.jpg',
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                id: 2
+        },
+        {
+                title: 'Discount', url: 'https://i2.wp.com/blog.jumia.com.ng/wp-content/uploads/2017/11/Jumia-Black-Friday-Flash-Sale-20th-Nov-780x406.png?resize=780%2C406',
+                description: "Lorem Ipsum is simply discount text of the printing and typesetting industry.",
+                id: 3
+        }]
+        
