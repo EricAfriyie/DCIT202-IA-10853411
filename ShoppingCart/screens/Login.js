d5b02018d20d4307da8422274ccff2a157cf6ac5
Eric Afriyie <62954159+EricAfriyie@users.noreply.github.com>
2021-11-17 14:04:26 -0800

    Update Login.js

--- ShoppingCart/screens/Login.js
+++ ShoppingCart/screens/Login.js
@@ -1,96 +1,162 @@
-import * as React from "react";
-import { View, Text, Image, StatusBar,TextInput, TouchableOpacity,StyleSheet,ImageBackground,Dimensions } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+import * as React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+  Dimensions,
+} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
-} from 'react-native-responsive-screen'; 
-
+} from 'react-native-responsive-screen';
 
 export default function Login(params) {
   const navigation = params.navigation;
   return (
-    <View style={{ flex:1}}>
-    <View>
-     <Text style={{ color: '#080A50', fontFamily: 'monospace',fontWeight:"bolder",fontSize:43,textAlign:"center"}}>Kew<Text style={{color:"#f03e3e",fontSize:43}}>s</Text></Text></View>
-  
-    
-        <StatusBar hidden={true} />
-        <View style={{flexDirection:"row"}}>
+    <View style={{ flex: 1 }}>
+      <View>
+        <Text
+          style={{
+            color: '#080A50',
+            fontFamily: 'sans-serif-medium',
+            fontWeight: 'bolder',
+            fontSize: 40,
+            marginTop: 15,
+            textAlign: 'center',
+            textShadowColor: '#000',
+            textShadowOffset: { width: 0.7, height: 0.8 },
+            textShadowRadius: 4,
+          }}>
+          Kew<Text style={{ color: '#f03e3e', fontSize: 40 }}>s</Text>
+        </Text>
+      </View>
 
-         <View style={styles.container2} >
-        <Image source={require('./pictures/phonesale.png')} style={{width:50,height:70,marginBottom:40, alignSelf:"center",marginLeft:40}}/>
+      <StatusBar hidden={true} />
+      <View style={{ flexDirection: 'row' }}>
+        <View style={styles.container2}>
+          <Image
+            source={require('./pictures/phonesale.png')}
+            style={{
+              width: 50,
+              height: 70,
+              marginBottom: 40,
+              alignSelf: 'center',
+              marginLeft: 40,
+            }}
+          />
         </View>
 
         <View style={styles.container3}>
-        <Image source={require('./pictures/phonerepair1.png')} style={{width:120,height:75,marginRight:30, alignSelf:"center", }}/>
-        </View></View>
-
-        <View style={styles.container1}>
-        <Image source={require('./pictures/pngegg.png')} style={{width:305,height:230, alignSelf:"center", marginTop:30}}/>
+          <Image
+            source={require('./pictures/phonerepair1.png')}
+            style={{
+              width: 120,
+              height: 75,
+              marginRight: 30,
+              alignSelf: 'center',
+            }}
+          />
         </View>
+      </View>
 
-        <Text style={{ color: '#080A50', fontWeight:"bolder",fontSize:30,textAlign:"center"}}>Find your latest &</Text>
-        <Text style={{ color: '#080A50', fontWeight:"bolder",fontSize:20,textAlign:"center"}}> Stylish Gadgets here </Text>
-        
-        
-     <TouchableOpacity
+      <View style={styles.container1}>
+        <Image
+          source={require('./pictures/pngegg.png')}
+          style={{
+            width: 305,
+            height: 230,
+            alignSelf: 'center',
+            marginTop: 30,
+          }}
+        />
+      </View>
+
+      <Text
+        style={{
+          color: '#080A50',
+          fontWeight: 'bolder',
+          fontSize: 30,
+          textAlign: 'center',
+        }}>
+        Find your latest &
+      </Text>
+      <Text
+        style={{
+          color: '#080A50',
+          fontWeight: 'bolder',
+          fontSize: 20,
+          textAlign: 'center',
+        }}>
+        {' '}
+        Stylish Gadgets here{' '}
+      </Text>
+
+      <TouchableOpacity
         onPress={() => {
-          navigation.navigate("Home");
+          navigation.navigate('StartScreen');
         }}
         style={{
-          alignSelf:"center",
+          alignSelf: 'center',
           padding: 6,
           paddingHorizontal: 70,
-          width:290,
+          width: 290,
           marginTop: 30,
-          alignItems: "center",
+          alignItems: 'center',
           borderRadius: 10,
-          flexDirection: "row",
-          backgroundColor: "#080A50",
-        }}
-      >
-      
-        <Text style={{ color: "white" ,alignSelf:"center",textAlign:"center", fontSize:23,}}>Get started</Text>
+          flexDirection: 'row',
+          backgroundColor: '#080A50',
+        }}>
+        <Text
+          style={{
+            color: 'white',
+            alignSelf: 'center',
+            textAlign: 'center',
+            fontSize: 23,
+          }}>
+          Get started
+        </Text>
       </TouchableOpacity>
     </View>
   );
 }
 
-
-
 const styles = StyleSheet.create({
-  
   container1: {
     backgroundColor: '#080A50',
     height: hp('35%'),
     width: wp('74%'),
-   alignSelf:"center",
-    marginTop:20,
-    marginBottom:90,
-    borderRadius:500,
-   
+    alignSelf: 'center',
+    marginTop: 20,
+    marginBottom: 90,
+    borderRadius: 500,
   },
 
-   container2: {
+  container2: {
     backgroundColor: '#080A50',
     height: hp('7%'),
     width: wp('14%'),
-    marginRight:150,
-    marginLeft:50,
-   alignSelf:"center",
-    borderRadius:500,
-   
+    marginRight: 150,
+    marginLeft: 50,
+
+    alignSelf: 'center',
+
+    borderRadius: 500,
   },
 
-   container3: {
+  container3: {
     backgroundColor: '#080A50',
     height: hp('7%'),
     width: wp('14%'),
-    marginTop:60,
-   alignSelf:"center",
-    borderRadius:500,
-   
-  }
-  
+    marginTop: 60,
+
+    alignSelf: 'center',
+
+    borderRadius: 500,
+  },
 });
